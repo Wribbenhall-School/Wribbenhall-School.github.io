255b97784a355e701967a243c553e68e50a80b51
David <davidjcottrell@icloud.com>
2021-05-24 16:08:23 +0100

    update

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7b8f13d8ae6def65bab30eaf2fbbc464.js"
+  "/precache-manifest.0ee73c0e2cb97e77d093ed6e869aea19.js"
 );
 
 workbox.clientsClaim();
